20d3de4b40126f53c49501b3bc051971ebbcc577
Sidney Gijzen <info@puzzlingpanda.nl>
2023-12-15 18:09:14 +0100

    Improve test setup

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -16,6 +16,28 @@ global.matchMedia =
     }
   }
 
+// Leaflet uses SVG renderer which JSDOM does not support SVG to a full extent.
+// In particular createSVGRect is not supported.
+// thanks to this SO answer: https://stackoverflow.com/a/54384719/1908609
+let createElementNSOrig = global.document.createElementNS
+// @ts-ignore
+global.document.createElementNS = function (
+  namespaceURI: string,
+  qualifiedName: string,
+) {
+  if (
+    namespaceURI === 'http://www.w3.org/2000/svg' &&
+    qualifiedName === 'svg'
+  ) {
+    let element = createElementNSOrig.apply(this, [namespaceURI, qualifiedName])
+    // @ts-ignore
+    element.createSVGRect = function () {}
+    return element
+  }
+
+  return createElementNSOrig.apply(this, [namespaceURI, qualifiedName])
+}
+
 // Establish API mocking before all tests.
 beforeAll(() => {
   server.listen({
