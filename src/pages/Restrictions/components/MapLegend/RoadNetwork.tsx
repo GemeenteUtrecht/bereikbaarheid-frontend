b45ad39dad488f5ec809dacfd25103c593ff62d7
Sidney Gijzen <info@puzzlingpanda.nl>
2024-02-19 18:09:12 +0100

    Restrictions: reflect active thematic layer in no restrictions label

--- src/pages/Restrictions/components/MapLegend/RoadNetwork.tsx
+++ src/pages/Restrictions/components/MapLegend/RoadNetwork.tsx
@@ -6,13 +6,30 @@ import {
   LegendItemsWrapper,
 } from '../../../../shared/components/MapLegendStyles'
 
+import { layerIds } from '../../contexts/mapLayersReducer'
+import { useRestrictionsMapContext } from '../../contexts/MapContext'
+
+const vehiclePropertyToLabel: Record<(typeof layerIds)[number], string> = {
+  vehicleAxleWeight: 'aslast',
+  vehicleWidth: 'breedte',
+}
+
 export const RestrictionsMapLegendRoadNetwork = () => {
+  const { activeMapLayers } = useRestrictionsMapContext()
+  const noRestrictionsLabel = () => {
+    const activeLayer = (
+      Object.keys(activeMapLayers) as (keyof typeof activeMapLayers)[]
+    ).find(key => activeMapLayers[key])
+
+    return `geen ${vehiclePropertyToLabel[activeLayer!]}beperking`
+  }
+
   return (
     <>
       <LegendWrapper>
         <CompactThemeProvider>
           <LegendItemsWrapper>
-            <MapLegendItem color="#60b20d" text="geen ontheffing nodig" />
+            <MapLegendItem color="#60b20d" text={noRestrictionsLabel()} />
 
             <MapLegendItem color="#91522d" text="voetgangersgebied" />
           </LegendItemsWrapper>
