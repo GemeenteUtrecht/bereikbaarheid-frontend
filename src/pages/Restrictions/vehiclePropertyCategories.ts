b9ef928b3cddde9587e5b6250ebef49bed038d18
Sidney Gijzen <info@puzzlingpanda.nl>
2024-02-20 18:06:26 +0100

    Restrictions: use the same color palette across all thematic layers

--- src/pages/Restrictions/vehiclePropertyCategories.ts
+++ src/pages/Restrictions/vehiclePropertyCategories.ts
@@ -5,16 +5,32 @@
  */
 import type { VehiclePropertyCategory } from './types/vehiclePropertyCategory'
 
+// https://mk.bcgsc.ca/colorblind/palettes.mhtml#12-color-palette-for-colorbliness
+const colors = [
+  '#9f0162',
+  '#ff5aaf',
+  '#009f81',
+  '#008df9',
+  '#ffc33b',
+  '#8400cd',
+  '#e20134',
+  '#ff6e3a',
+  '#ffb2fd',
+  '#00fccf',
+  '#a40122',
+  '#00c2f9',
+]
+
 export const axleWeightCategories: VehiclePropertyCategory[] = [
-  { color: 'rgb(197,59,47)', label: '9 t', value: 9001 },
-  { color: 'rgb(229, 153, 93)', label: '8 t', value: 8001 },
-  { color: 'rgb(0, 195, 169)', label: '5 t', value: 5001 },
-  { color: 'rgb(252, 132, 173)', label: '4 t', value: 4001 },
-  { color: 'rgb(0, 186, 249)', label: '3,5 t', value: 3501 },
-  { color: 'rgb(185, 157, 250)', label: '2 t', value: 2001 },
+  { color: colors[0], label: '9 t', value: 9001 },
+  { color: colors[1], label: '8 t', value: 8001 },
+  { color: colors[2], label: '5 t', value: 5001 },
+  { color: colors[3], label: '4 t', value: 4001 },
+  { color: colors[4], label: '3,5 t', value: 3501 },
+  { color: colors[5], label: '2 t', value: 2001 },
 ]
 
 export const widthCategories: VehiclePropertyCategory[] = [
-  { color: '#da3417', label: '2,3 m', value: 2.31 },
-  { color: '#f59309', label: '2,1 m', value: 2.11 },
+  { color: colors[0], label: '2,3 m', value: 2.31 },
+  { color: colors[1], label: '2,1 m', value: 2.11 },
 ]
